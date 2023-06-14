3949dbe352eb1bba55cc6b6e2c269450172feac6
ToeDev <73237279+ToeDev@users.noreply.github.com>
2023-06-14 14:24:25 -0400

    Update SignSet.java

--- src/main/java/org/cubeville/cvbasicnbt/commands/sign/SignSet.java
+++ src/main/java/org/cubeville/cvbasicnbt/commands/sign/SignSet.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+import org.bukkit.block.sign.Side;
 import org.bukkit.entity.Player;
 import org.bukkit.block.Sign;
 
@@ -26,6 +27,7 @@ public class SignSet extends CommandWithSign
         addOptionalBaseParameter(new CommandParameterString());
         addOptionalBaseParameter(new CommandParameterString());
         addOptionalBaseParameter(new CommandParameterString());
+        addParameter("side", true, new CommandParameterString());
     }
 
     @Override
@@ -34,11 +36,22 @@ public class SignSet extends CommandWithSign
 
         int firstLine = (int) baseParameters.get(0);
 
-        if(firstLine < 1 || firstLine + baseParameters.size() > 6)
+        if(firstLine < 1 || firstLine + baseParameters.size() > 6) {
             throw new CommandExecutionException("Invalid line number!");
+        }
+        Side side = Side.FRONT;
+        if(parameters.containsKey("side")) {
+            if(parameters.get("side").toString().equalsIgnoreCase("front")) {
+                side = Side.FRONT;
+            } else if(parameters.get("side").toString().equalsIgnoreCase("back")) {
+                side = Side.BACK;
+            } else {
+                throw new CommandExecutionException("Invalid side! Use side:front or side:back!");
+            }
+        }
 
         for(int i = 0; i < baseParameters.size() - 1; i++) {
-            sign.setLine(i + firstLine - 1, ColorUtils.addColorWithoutHeader((String) baseParameters.get(i + 1)));
+            sign.getSide(side).setLine(i + firstLine - 1, ColorUtils.addColorWithoutHeader((String) baseParameters.get(i + 1)));
         }
 
         return new CommandResponse("&aSign text changed.");
